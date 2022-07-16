4574c861277b2412f173d3386be7628552c4e4c4
duynghia-123 <duynghia22302@gmail.com>
2022-07-16 15:12:38 +0700

    update-title

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -7,13 +7,6 @@ const Weather = () => {
       <p>
         Enter city and country or select from one of the cities below to find
         out current weather conditions. Source code:{" "}
-        <a
-          href="https://github.com/duynghia-123/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub repo
-        </a>
       </p>
       <p>
         <a
