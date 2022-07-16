ddd95af544a42a5926c420d3827cd7b21b5614e7
DuyNghia <72549676+duynghia-123@users.noreply.github.com>
2022-07-16 15:16:20 +0700

    Update Titles.js

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -6,7 +6,7 @@ const Weather = () => {
       <h1>Current Weather</h1>
       <p>
         Enter city and country or select from one of the cities below to find
-        out current weather conditions. Source code:{" "}
+        out current weather conditions.
       </p>
       <p>
         <a
